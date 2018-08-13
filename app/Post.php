cd7306e7830d515f2f07bb78978729f1f3100083
lal <eduardo.rasgado@hotmail.com>
2018-08-13 00:44:06 -0500

    post model, migration and relationships

---
+++ app/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+	protected $fillable = [
+		'body'
+	]
+	public function user()
+	{
+		// one post belongs to a user
+		return $this->belongsTo(User::class)
+	}
+}
