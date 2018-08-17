7075041193f17ff8123495886ea3a4b1fb5a5751
lal <eduardo.rasgado@hotmail.com>
2018-08-16 20:21:00 -0500

    unfollow method implemented in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,18 +25,29 @@ class UserController extends Controller
     	if ($request->user()->canFollow($user)) 
     	{
     		/*
-    		 For example, let's imagine a user can have many roles and a role can have many users. To attach a role to a user by inserting a record in the intermediate table that joins the models
+    		 For example, let's imagine a user can have many roles and a role can have many users. To attach a role to a user by inserting a record in the intermediate table that joins the models.
     		 See:
     		 https://laravel.com/docs/5.6/eloquent-relationships
     		*/
     		 // following method in User model
     		$request->user()->following()->attach($user->id);
     	}
+    	/*
+			Sometimes you may wish to redirect the user to their previous location, such as when a submitted form is invalid. You may do so by using the global back helper function. Since this feature utilizes the session, make sure the route calling the back function is using the web middleware group or has all of the session middleware applied.
+			See:
+			https://laravel.com/docs/5.6/redirects
+    	*/
     	return redirect()->back();
     }
 
     public function unfollow(Request $request, User $user)
     {
-    	dd($user);
+    	// check if can unfollow
+    	if ($request->user()->canUnfollow($user))
+    	{
+    		// detach is the opposite to attach method
+    		$request->user()->following()->detach($user->id);
+    	}
+    	return redirect()->back();
     }
 }
